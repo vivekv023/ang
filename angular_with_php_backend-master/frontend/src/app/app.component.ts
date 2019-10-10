47221c45d17be032f0c89c066a51bafc0bae86aa
vivek <pathakv023@gmail.com>
2019-10-10 11:17:48 +0530

    my first git commit command

---
+++ angular_with_php_backend-master/frontend/src/app/app.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Car } from './car';
+import { CarService } from './car.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  cars: Car[];
+  error = '';
+  success = '';
+
+  car = new Car('', 0);
+
+  constructor(private carService: CarService) {
+  }
+
+  ngOnInit() {
+    this.getCars();
+  }
+
+  getCars(): void {
+    this.carService.getAll().subscribe(
+      (res: Car[]) => {
+        this.cars = res;
+      },
+      (err) => {
+        this.error = err;
+      }
+    );
+  }
+
+  addCar(f) {
+    this.resetErrors();
+
+    this.carService.store(this.car)
+      .subscribe(
+        (res: Car[]) => {
+          // Update the list of cars
+          this.cars = res;
+
+          // Inform the user
+          this.success = 'Created successfully';
+
+          // Reset the form
+          f.reset();
+        },
+        (err) => this.error = err
+      );
+  }
+
+  updateCar(name, price, id) {
+    this.resetErrors();
+
+    this.carService.update({ model: name.value, price: price.value, id: +id })
+      .subscribe(
+        (res) => {
+          this.cars    = res;
+          this.success = 'Updated successfully';
+        },
+        (err) => this.error = err
+      );
+  }
+
+  deleteCar(id) {
+    this.resetErrors();
+
+    this.carService.delete(+id)
+      .subscribe(
+        (res: Car[]) => {
+          this.cars = res;
+          this.success = 'Deleted successfully';
+        },
+        (err) => this.error = err
+      );
+  }
+
+  private resetErrors(){
+    this.success = '';
+    this.error   = '';
+  }
+
+}
